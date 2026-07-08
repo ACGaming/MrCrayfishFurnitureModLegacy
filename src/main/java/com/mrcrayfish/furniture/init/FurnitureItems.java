e5c6dd4a539cce1cd19ac530696cd90f9566ed39
MisterIceCat <mistericecat@gmail.com>
2026-07-08 14:22:47 +0200

    DDR5 Ram Module

--- src/main/java/com/mrcrayfish/furniture/init/FurnitureItems.java
+++ src/main/java/com/mrcrayfish/furniture/init/FurnitureItems.java
@@ -75,6 +75,7 @@ public class FurnitureItems
     public static final Item SCREEN;
     public static final Item CIRCUIT_BOARD;
     public static final Item CONTROLLER_UNIT;
+    public static final Item DDR5_RAM;
     public static final Item WIFI_CARD;
     public static final Item GOLDEN_WIRE;
     public static final Item PRINTER_CARRIAGE;
@@ -140,6 +141,7 @@ public class FurnitureItems
         SCREEN = new ItemGeneric().setTranslationKey("item_screen").setRegistryName("item_screen");
         CIRCUIT_BOARD = new ItemGeneric().setTranslationKey("item_circuit_board").setRegistryName("item_circuit_board");
         CONTROLLER_UNIT = new ItemGeneric().setTranslationKey("item_controller_unit").setRegistryName("item_controller_unit");
+        DDR5_RAM = new ItemGeneric().setTranslationKey("item_ddr5_ram").setRegistryName("item_ddr5_ram");
         WIFI_CARD = new ItemGeneric().setTranslationKey("item_wifi_card").setRegistryName("item_wifi_card");
         GOLDEN_WIRE = new ItemGeneric().setTranslationKey("item_golden_wire").setRegistryName("item_golden_wire");
         PRINTER_CARRIAGE = new ItemGeneric().setTranslationKey("item_printer_carriage").setRegistryName("item_printer_carriage");
@@ -200,6 +202,7 @@ public class FurnitureItems
         registerItem(SCREEN);
         registerItem(CIRCUIT_BOARD);
         registerItem(CONTROLLER_UNIT);
+        registerItem(DDR5_RAM);
         registerItem(WIFI_CARD);
         registerItem(GOLDEN_WIRE);
         registerItem(PRINTER_CARRIAGE);
